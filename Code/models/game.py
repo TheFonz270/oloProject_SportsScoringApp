e3a52731c5b1df1146669da8e72a3d1d3f31c6ec
Martin Quinn <thefonz270@hotmail.com>
2021-11-14 13:39:50 +0000

    reconcile game method created and tested

--- Code/models/game.py
+++ Code/models/game.py
@@ -9,5 +9,22 @@ class Game():
     def input_score(self, score1, score2):
         self.score = [score1, score2]
     
-    def reconcile_game():
+    def reconcile_game(self):
+        # set game to completed
+        self.completed = True
+        # identify which player won 
+        if self.score[0] > self.score[1]:
+        # give the player that won a win
+            self.player1.winner()
+        # give the player that lost a loss 
+            self.player2.loser()
+        elif self.score[0] < self.score[1]:
+            self.player1.loser()
+            self.player2.winner()
+        # assign the pts for and againsy for each player
+        self.player1.pts_for = self.score[0]
+        self.player2.pts_against = self.score[0]
+        self.player1.pts_against = self.score[1]
+        self.player2.pts_for = self.score[1]
         pass
+            
--- Code/tests/game_tests.py
+++ Code/tests/game_tests.py
@@ -22,3 +22,15 @@ class TestGame(unittest.TestCase):
     def test_input_game_score(self):
         self.game.input_score(70, 30)
         self.assertEqual([70,30], self.game.score)
+
+    def test_game_reconciliation(self):
+        self.game.input_score(70, 30)
+        self.game.reconcile_game()
+        self.assertEqual(1,self.player1.wins)
+        self.assertEqual(1,self.player2.losses)
+        self.assertEqual(70,self.player1.pts_for)
+        self.assertEqual(70,self.player2.pts_against)
+        self.assertEqual(30,self.player1.pts_against)
+        self.assertEqual(30,self.player2.pts_for)
+        self.assertEqual(True, self.game.completed)
+
